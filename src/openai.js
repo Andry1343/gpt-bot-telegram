fa8d8e74f48582f428d6b3d95f5f76cdfcfbc903
Andry1343 <ua.gavrilets@gmail.com>
2023-05-16 23:57:34 +0300

    Update openai.js

--- src/openai.js
+++ src/openai.js
@@ -1,5 +1,55 @@
 import { Configuration, OpenAIApi } from "openai";
 import { createReadStream } from "fs";
+import dotenv from "dotenv";
+dotenv.config();
+
+class OpenAI {
+  roles = {
+    ASISTANT: 'assistant',
+    USER: 'user',
+    SYSTEM: 'system',
+  };
+
+  constructor(apiKey) {
+    const configuration = new Configuration({
+      apiKey,
+    });
+    this.openai = new OpenAIApi(configuration);
+  }
+
+  async chat(messages) {
+    try {
+      const response = await this.openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages,
+      });
+      return response.data.choices[0].message;
+    } catch (error) {
+      console.log("Error while writing gpt chat", error.message);
+      throw error;
+    }
+  }
+
+  async transcription(filepath) {
+    try {
+      const response = await this.openai.createTranscription(
+        createReadStream(filepath),
+        'whisper-1'
+      );
+      return response.data.text;
+    } catch (error) {
+      console.log("Error while transcription", error.message);
+      throw error;
+    }
+  }
+}
+
+export const openai = new OpenAI(process.env.OPENAI_API_KEY);
+
+
+
+/* import { Configuration, OpenAIApi } from "openai";
+import { createReadStream } from "fs";
 import dotenv from "dotenv"
 dotenv.config();
 
@@ -43,3 +93,4 @@ class OpenAI {
 }
 
 export const openai = new OpenAI(process.env.OPENAI_API_KEY);
+ */
