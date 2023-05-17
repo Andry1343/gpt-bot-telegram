e7457ea87302b2a1771f9e86a6ad6a55a929a921
Andry1343 <ua.gavrilets@gmail.com>
2023-05-18 02:45:12 +0300

    Delete comment

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import { message } from "telegraf/filters";
 import { ogg } from "./ogg.js";
 import { openai } from "./openai.js";
 import dotenv from "dotenv";
-// import { createReadStream } from "fs";
 dotenv.config();
 
 const INITIAL_SESSION = {
@@ -81,89 +80,6 @@ try {
   console.error('Error occurred while launching the bot', error);
 }
 
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
-
-
-
-/* import { Telegraf, session } from "telegraf";
-import { code } from "telegraf/format";
-import { message } from "telegraf/filters"; // іморт необхідних фільтрів
-import { ogg } from "./ogg.js";
-import { openai } from "./openai.js";
-import dotenv from "dotenv"
-dotenv.config();
-
-
-const INITIAL_SESSION = {
-  messages: [],
-};
-
-const bot = new Telegraf(process.env.TELEGRAM_TOKEN);
-
-bot.use(session());
-
-bot.command("new", async (ctx) => {
-  ctx.session = INITIAL_SESSION;
-  await ctx.reply("Чекаю на ваше нове питання");
-});
-
-bot.command("start", async (ctx) => {
-  ctx.session = INITIAL_SESSION;
-  await ctx.reply("Чекаю на ваше нове питання");
-});
-
-bot.on(message("voice"), async (ctx) => {
-  ctx.session = ctx.session ?? INITIAL_SESSION;
-  try {
-    await ctx.reply(code("Чекаю відповідь від сервера..."));
-    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id); //
-    const userId = String(ctx.message.from.id); // зберігає ід користувача який взаємодіє з чатом (розрізняти користувачів)
-    const oggPath = await ogg.create(link.href, userId);
-    const mp3Path = await ogg.toMp3(oggPath, userId);
-
-    const text = await openai.transcription(mp3Path);
-    await ctx.reply(code(`Ваш запит: ${text}`));
-
-    ctx.session.messages.push({ role: openai.roles.USER, content: text });
-
-    const response = await openai.chat(ctx.session.messages);
-
-    ctx.session.messages.push({
-      role: openai.roles.ASISTANT,
-      content: response.content,
-    });
-
-    await ctx.reply(response.content);
-  } catch (e) {
-    console.log(`Error while voice message`, e.message);
-  }
-});
-
-bot.on(message("text"), async (ctx) => {
-  ctx.session = ctx.session ?? INITIAL_SESSION;
-  try {
-    await ctx.reply(code("Чекаю відповідь від сервера..."));
-
-    ctx.session.messages.push({
-      role: openai.roles.USER,
-      content: ctx.message.text,
-    });
-
-    const response = await openai.chat(ctx.session.messages);
-
-    ctx.session.messages.push({
-      role: openai.roles.ASISTANT,
-      content: response.content,
-    });
-
-    await ctx.reply(response.content);
-  } catch (e) {
-    console.log(`Error while voice message`, e.message);
-  }
-});
-
-bot.launch();
-
 process.once("SIGINT", () => bot.stop("SIGINT")); // потрібно для того щоб зупинити бота за подією 'SIGINT'
-process.once("SIGTERM", () => bot.stop("SIGTERM")); // потрібно для того щоб зупинити бота за подією 'SIGTERM' */
+process.once("SIGTERM", () => bot.stop("SIGTERM")); // потрібно для того щоб зупинити бота за подією 'SIGTERM'
+
